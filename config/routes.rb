c21e53636488505b5bb22e4d34b4d312218e5845
DerBaumann <natur5210@gmail.com>
2026-09-21 15:50:00 +0200

    Run the authentication generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root to: "recipes#index"
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

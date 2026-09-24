1ed48c2ada0e6a5f09a767f76c8534bd04e89d0e
DerBaumann <natur5210@gmail.com>
2026-09-24 11:06:28 +0200

    Add favourites#index page. rename _recipe to _card and change logic and markup to make toggle btn work in both favourites#index and recipes#index

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -14,7 +14,24 @@ class FavouritesController < ApplicationController
   def destroy
     Current.user.favourites.find_by(recipe: @recipe)&.destroy
 
-    redirect_back_or_to @recipe
+    # redirect_back_or_to @recipe
+
+    respond_to do |format|
+      format.html { redirect_back_or_to recipes_path }
+      format.turbo_stream {
+        render turbo_stream: [
+          # Wird NUR auf der Favoriten-Seite ausgeführt (wo :favourite_card existiert):
+          turbo_stream.remove(helpers.dom_id(@recipe, :favourite_card)),
+
+          # Wird ÜBERALL ausgeführt (Detail- & Rezept-Übersicht):
+          turbo_stream.replace(
+            helpers.dom_id(@recipe, :favourite_button),
+            partial: "favourites/toggle_button",
+            locals: { recipe: @recipe }
+          )
+        ]
+      }
+    end
   end
 
   private
